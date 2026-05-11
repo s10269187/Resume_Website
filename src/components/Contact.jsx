170c1a91f6c8538917642af33fefc4354eb55947
s10269187 <leejialuuuu@gmail.com>
2026-05-11 11:41:18 +0800

    Revert contact cards to 3-column grid

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -52,7 +52,7 @@ export default function Contact() {
 
         <div className="max-w-3xl mx-auto">
           {/* Contact cards */}
-          <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-5 mb-12">
+          <div className="grid sm:grid-cols-3 gap-5 mb-12">
             {contactItems.map(item => (
               <a
                 key={item.label}
